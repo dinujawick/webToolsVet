721b8d1a181cc5d0e17e26666f962b027b1b437b
Dinuja S'Wickrama <dinujawilligodaliyanage@unisa.edu.au>
2022-05-19 18:07:58 +0930

    fix the issue with wron selection

--- pax-main-js.js
+++ pax-main-js.js
@@ -112,7 +112,7 @@ function addMed(selectedMedDetails) {
             $('<td>').text(selectedMedDetails.recode_effect_on_concentration_1 + selectedMedDetails.recode_effect_on_concentration_2).appendTo($tableRow);
         } else {
             $('<td>').text(selectedMedDetails.recode_effect_on_concentration_1).appendTo($tableRow);
-            const temp = selectedMedDetails.recode_effect_on_concentration_2.split("|");
+            const temp = selectedMedDetails.recode_effect_on_concentration_1.split("|");
             console.log(temp);
         }
 
