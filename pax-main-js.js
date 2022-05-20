93cff72aa8fbb74b05308505b91bd47624c2562b
Dinuja S'Wickrama <dinujawilligodaliyanage@unisa.edu.au>
2022-05-20 09:54:59 +0930

    fix the id

--- pax-main-js.js
+++ pax-main-js.js
@@ -118,7 +118,7 @@ function addMed(selectedMedDetails) {
                     console.log(temp[0]);
                     //Add up arrow icon
                     $('<td>').append(
-                        $('<span>').addClass('arrowClass').append(
+                        $('<span>').attr('id','tblArrows').append(
                             $('<svg>')
                                 .attr('xmlns', "http://www.w3.org/2000/svg")
                                 .attr('width', "16")
