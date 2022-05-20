55ad68e2dc58d701283988b954d6d4c18f61e4f1
Dinuja S'Wickrama <dinujawilligodaliyanage@unisa.edu.au>
2022-05-20 09:37:07 +0930

    fix the bug

--- pax-main-js.js
+++ pax-main-js.js
@@ -118,7 +118,7 @@ function addMed(selectedMedDetails) {
                     console.log(temp[0]);
                     //Add up arrow icon
                     $('<td>').append(
-                        $('<p>').text(temp[1]).append(
+                        $('<span>').append(
                             $('<svg>')
                                 .attr('xmlns', "http://www.w3.org/2000/svg")
                                 .attr('width', "16")
@@ -129,8 +129,9 @@ function addMed(selectedMedDetails) {
                                     $('<path>')
                                         .attr('fill-rule', "evenodd")
                                         .attr('d', "M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z")
-                            )
-                        )
+                                )
+                        ),
+                        $('<span>').text(temp[1])
                     ).appendTo($tableRow);
                     break;
                 case "<down>": console.log(temp[0]); break;
