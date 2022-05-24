342fd9e242e38d923c58a17e5c56d053a94db90d
Dinuja S'Wickrama <dinujawilligodaliyanage@unisa.edu.au>
2022-05-24 13:47:15 +0930

    check query string

--- pax-main-js.js
+++ pax-main-js.js
@@ -62,6 +62,23 @@ $('#btnPrint').on('click', function (event) {
 
 $(document).ready(function () {
 
+
+
+    var name = GetParameterValues('Name');
+    var id = GetParameterValues('ID');
+
+    alert("Hello " + name + " your ID is " + id);
+
+    function GetParameterValues(param) {
+        var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for (var i = 0; i < url.length; i++) {
+            var urlparam = url[i].split('=');
+            if (urlparam[0] == param) {
+                return urlparam[1];
+            }
+        }
+    }
+
     //***************************************************Card Way******************************************************
     //Create Card Deck
     $cardDeck = $('<div>').attr('id', 'mainDeck').addClass('row row-cols-1 row-cols-md-2 g-4').appendTo($('#divMedCon'));
