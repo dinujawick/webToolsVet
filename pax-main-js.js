1f8c9bfb1228227bceb26c9009a9758f8785b32f
Dinuja S'Wickrama <dinujawilligodaliyanage@unisa.edu.au>
2022-05-24 14:01:59 +0930

    debugging

--- pax-main-js.js
+++ pax-main-js.js
@@ -71,6 +71,7 @@ $(document).ready(function () {
 
     function GetParameterValues(param) {
         var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        console.log(url);
         for (var i = 0; i < url.length; i++) {
             var urlparam = url[i].split('=');
             if (urlparam[0] == param) {
