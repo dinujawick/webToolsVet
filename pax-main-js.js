2dc53ac72478a2e6123d426ca5f9340dc5ded43d
Dinuja S'Wickrama <dinujawilligodaliyanage@unisa.edu.au>
2022-05-24 10:53:09 +0930

    fix the card layout into 2 cols

--- pax-main-js.js
+++ pax-main-js.js
@@ -64,7 +64,7 @@ $(document).ready(function () {
 
     //***************************************************Card Way******************************************************
     //Create Card Deck
-    $cardDeck = $('<div>').attr('id', 'mainDeck').addClass('row row-cols-1 row-cols-md-3 g-4').appendTo($('#divMedCon'));
+    $cardDeck = $('<div>').attr('id', 'mainDeck').addClass('row row-cols-1 row-cols-md-2 g-4').appendTo($('#divMedCon'));
     //*****************************************************************************************************************
 
 
