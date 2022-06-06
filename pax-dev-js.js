8011aee3adf37dee3459197f9da54c2def1738a4
Dinuja S'Wickrama <dinujawilligodaliyanage@unisa.edu.au>
2022-06-06 16:04:56 +0930

    fix the bug

--- pax-dev-js.js
+++ pax-dev-js.js
@@ -112,7 +112,7 @@ $(document).ready(function () {
 
                     setTimeout(function () { $('#alertBoxForNewMed').fadeOut(1000); }, 1000);
 
-                    this.addClass('disabled');
+                    $('<a>').attr('id', item.atc_level).addClass('disabled');
                 });
 
                 prevAddedItem = item.generic_name;
