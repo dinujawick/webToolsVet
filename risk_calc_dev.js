8bbfab6d3673307156040d8605064e72db18fb82
Dinuja S'Wickrama <dinujawilligodaliyanage@unisa.edu.au>
2022-07-14 11:20:21 +0930

    debugging

--- risk_calc_dev.js
+++ risk_calc_dev.js
@@ -616,6 +616,8 @@ function StackedBarChartHorizontal(data, {
         .attr("y", ({ i }) => yScale(Y[i]))
         .attr("width", ([x1, x2]) => Math.abs(xScale(x1) - xScale(x2)))
         .attr("height", yScale.bandwidth());
+        ([x1, x2]) => Math.min(xScale(x1), xScale(x2));
+        console.log();
 
     if (title) bar.attr('data-bs-toggle', 'tooltip').attr('data-bs-placement','right').attr("title", ({ i }) => title(i));
 
