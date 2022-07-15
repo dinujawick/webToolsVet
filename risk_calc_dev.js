4b23977bc228934af9ee29fd2983d831b7bc0e9d
Dinuja S'Wickrama <dinujawilligodaliyanage@unisa.edu.au>
2022-07-15 13:40:38 +0930

    fixed the bug

--- risk_calc_dev.js
+++ risk_calc_dev.js
@@ -652,7 +652,7 @@ function StackedBarChartHorizontal(data,after, {
 
         });
 
-        const cappedBar = bar.append('g').attr('id', 'cappedBars');
+        const cappedBar = svg.append('g').attr('id', 'cappedBars');
 
         const caXScale = 530;
 
